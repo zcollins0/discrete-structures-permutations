c6b8019dd5492d73a96bf37e9bf325bf8ebd6c4e
Zach Collins <collinzy@mail.uc.edu>
2016-11-03 16:56:57 -0400

    init

---
+++ permutations.cxx
@@ -0,0 +1,15 @@
+/* Zachary Collins, Laura Tebben
+ * Discrete Structures Assignment 4
+ */
+
+#include <iostream>
+
+int main()
+{
+    int objs, perm;
+    std::cout << "Enter number of objects: ";
+    std::cin >> objs;
+    std::cout << "Enter permutation number: ";
+    std::cin >> perm;
+    return 0;
+}
