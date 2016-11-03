045b02a9da47ab28031746b4a18fc0a7da5964ee
Zach Collins <collinzy@mail.uc.edu>
2016-11-03 19:06:40 -0400

    make it almost work

--- permutations.py
+++ permutations.py
@@ -1,8 +1,22 @@
+import math
+
 def factorial(n):
     r = 1
     for i in range (1, n + 1):
         r *= i
     return r
+
+def get_perm(lst, perm):
+    sz = len(lst)
+    if sz == 1:
+        return lst[0]
+    fact = factorial(sz-1)
+    first = math.floor(perm/fact)
+    lstnew = lst[:]
+    lstnew.remove(lstnew[first])
+    if (perm >= sz - 1):
+        perm -= sz
+    return str(lst[first]) + str(get_perm(lstnew, perm))
 
 def main():
     sz = int(input("Enter the size of the list: "))
@@ -12,6 +26,8 @@ def main():
         print("The maximum permutation for a list of size", sz, "is", maxperm)
 
     numlist = list(range(1,sz+1))
+    permstr = get_perm(numlist, perm)
+    print("Permutation number", perm, "is", permstr)
 
 if __name__ == "__main__":
     main()
